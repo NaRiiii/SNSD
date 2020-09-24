9230a7fcc68340afd0094a5558b285a2a7ee446f
SunyoungLim <gsw0981@naver.com>
2020-09-24 13:48:48 +0900

    bean 설정(원장님 도움

--- src/main/java/kr/co/soccer/searchTeamMem/searchController.java
+++ src/main/java/kr/co/soccer/searchTeamMem/searchController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class searchController {
-	@GetMapping("search/searchTeam.sc")
+	@GetMapping("/search/searchTeam.sc")
 	public String register() {
 		return "search/searchTeam"; 
 	}
